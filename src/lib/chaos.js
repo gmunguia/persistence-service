9b08db74f156542c8b09b70e549ad08f1d4b7f0f
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2020-04-17 19:06:36 +0200

    fix error rate

--- src/lib/chaos.js
+++ src/lib/chaos.js
@@ -1,6 +1,5 @@
 const { Chaos } = require("./errors");
 
-module.exports.chaos = () => {
-  const ERROR_RATE = 1;
-  if (Math.random() < 1 / ERROR_RATE) throw new Chaos();
+module.exports.chaos = (errorRate = process.env.ERROR_RATE) => {
+  if (Math.random() < Number(errorRate)) throw new Chaos();
 };
